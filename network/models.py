44340ae4d8a799a27172356d88cfd61be42ecfc1
Marko Bondaruk <markobondaruk@gmail.com>
2023-09-22 21:35:05 -0600

    New post front-end finished

--- network/models.py
+++ network/models.py
@@ -4,3 +4,33 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    comments = models.ManyToManyField(User, related_name='commented_posts', blank=True)
+
+    def __str__(self):
+        return f"Post by {self.user.username} at {self.timestamp}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on post by {self.post.user.username}"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.post:
+            return f"Like by {self.user.username} on post by {self.post.user.username}"
+        elif self.comment:
+            return f"Like by {self.user.username} on comment by {self.comment.user.username}"
